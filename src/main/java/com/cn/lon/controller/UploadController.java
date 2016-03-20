6a9cf87b864564c0865ed7748aac03f0f14c5397
Qpm-wen <945766863@qq.com>
2016-03-20 10:50:10 +0800

    文件上传

--- src/main/java/com/cn/lon/controller/UploadController.java
+++ src/main/java/com/cn/lon/controller/UploadController.java
@@ -1,11 +1,24 @@
 package com.cn.lon.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * declaration： 
  *		上传文件controller
  * author wenkangqiang
  * date   2016年3月18日
  */
+@Controller
 public class UploadController {
+	
+	
+	@RequestMapping("/upload")
+	public ModelAndView uploadFile(){
+		
+		
+		return null;
+	}
 
 }
